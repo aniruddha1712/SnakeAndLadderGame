449cc472d35b4a6a0288d188c18681685f4bd7de
Aniruddha <aniruddha1712@gmail.com>
2022-03-30 15:29:36 +0530

    [Aniruddha]Added: Starting position of player

--- SnakeAndLadder/SnakeAndLadder.cs
+++ SnakeAndLadder/SnakeAndLadder.cs
@@ -11,6 +11,10 @@ namespace SnakeAndLadder
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Snake And Ladder Game");
+
+            int startPosition = 0;
+            Console.WriteLine("Starting position of player1: " + startPosition);
+
             Console.ReadLine();
         }
     }
