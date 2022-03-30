c6c369071753aec25a70ad82fd0e5dcf92a825ea
Aniruddha <aniruddha1712@gmail.com>
2022-03-30 14:36:48 +0530

    [Aniruddha]Added: Welcome message

---
+++ SnakeAndLadder/SnakeAndLadder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    class SnakeAndLadder
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Snake And Ladder Game");
+            Console.ReadLine();
+        }
+    }
+}
