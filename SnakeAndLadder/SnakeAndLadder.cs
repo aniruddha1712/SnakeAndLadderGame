c4518705415202a5d7976991fcea800c5e1ca5b6
Aniruddha <aniruddha1712@gmail.com>
2022-03-30 16:32:50 +0530

    [Aniruddha]Added: checking option for no play, ladder and snake

--- SnakeAndLadder/SnakeAndLadder.cs
+++ SnakeAndLadder/SnakeAndLadder.cs
@@ -15,11 +15,29 @@ namespace SnakeAndLadder
         }
         public static void DiceRoll()
         {
+            const int IS_NO_PLAY = 0, IS_LADDER = 1, IS_SNAKE = 2;
             int newPosition = 0;
             Random random = new Random();
             int dice = random.Next(1, 7);
-            newPosition += dice;
-            Console.WriteLine("new position of player1:" + newPosition);
+            int option = random.Next(0, 3);
+            switch(option)
+            {
+                case IS_NO_PLAY:
+                    newPosition += 0;
+                    break;
+                case IS_LADDER:
+                    newPosition += dice;
+                    break;
+                case IS_SNAKE:
+                    newPosition -= dice;
+                    if(newPosition<=0)
+                    {
+                        newPosition = 0;
+                    }
+                    break;
+            }
+
+            Console.WriteLine("new position of player1: " + newPosition+ " got number "+dice+" in die and option "+option);
         }
 
         static void Main(string[] args)
