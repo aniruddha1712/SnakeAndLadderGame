d6c3481829b29a8c9282f13c591e39dfb512d89b
Aniruddha <aniruddha1712@gmail.com>
2022-03-30 15:59:11 +0530

    [Aniruddha]Added: Dice roll to get further position

--- SnakeAndLadder/SnakeAndLadder.cs
+++ SnakeAndLadder/SnakeAndLadder.cs
@@ -8,13 +8,25 @@ namespace SnakeAndLadder
 {
     class SnakeAndLadder
     {
+        public static void Start()
+        {
+            int startPosition = 0;
+            Console.WriteLine("Starting position of player1: " + startPosition);
+        }
+        public static void DiceRoll()
+        {
+            int newPosition = 0;
+            Random random = new Random();
+            int dice = random.Next(1, 7);
+            newPosition += dice;
+            Console.WriteLine("new position of player1:" + newPosition);
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Snake And Ladder Game");
-
-            int startPosition = 0;
-            Console.WriteLine("Starting position of player1: " + startPosition);
-
+            Start();
+            DiceRoll();
             Console.ReadLine();
         }
     }
